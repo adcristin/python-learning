b6472ad516f72c6ad87fa7f94fe7c2ad83614485
adcristin <dasabhishek01@outlook.com>
2026-01-12 23:04:10 +0530

    Added 2 files about Strings, libraries and built-in functions

---
+++ Libraries&BuiltInFunctions.py
@@ -0,0 +1,19 @@
+#There are several libraries in python and built-in functions which are used for different purposes and also for ease. 
+
+#square root
+import math 
+print(math.sqrt(64))
+
+#Area of circle 
+r = 2
+AOC = print(math.pi*r*r)
+
+#absolute value(built in function)
+print(abs(-99))
+
+#type(built in function)
+print(type(r))
+
+
+
+
---
+++ MoreWithStrings.py
@@ -0,0 +1,11 @@
+first = "Python"
+second = "Learning"
+
+#Adding two strings.
+last = first + " " + second 
+print("Added String:", last)
+
+#Accessing specific character or part from the string, through indexing. 
+print(first[0])
+print(last[:12])
+print(second[1:5])
